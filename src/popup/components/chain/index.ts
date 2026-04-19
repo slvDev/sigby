1b98fd12077183d9405865fb9971b4e6e5e39dff
slvDev <slava.serbov@gmail.com>
2026-04-19 18:48:31 +0300

    chore: drop old header, bottomnav, pagelayout and chain components

--- src/popup/components/chain/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Chain Components
- */
-
-export { ChainIcon } from "./ChainIcon";
-export { ChainItem } from "./ChainItem";
-export { ChainList } from "./ChainList";
-export { ChainSelector } from "./ChainSelector";
